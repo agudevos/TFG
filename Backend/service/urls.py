8b6902d73e85ff8e6e929ef935218cc426323e58
agudevbon <agudevbon@alum.us.es>
2025-03-31 13:15:52 +0200

    web scraping to get platform events

--- Backend/service/urls.py
+++ Backend/service/urls.py
@@ -1,4 +1,8 @@
 from django.urls import path
 from . import views
 
-urlpatterns = [path('services/create/', views.ServiceCreateView.as_view(), name='establishment_list'),]
+urlpatterns = [
+    path('services/create/', views.ServiceCreateView.as_view(), name='service_list'),
+    path('services/<int:pk>/', views.ServiceDetailView.as_view(), name='service_detail'),
+    path('services/events/<int:pk>/', views.ServiceEventsView.as_view(), name='service_events')
+]
--- Backend/service/views.py
+++ Backend/service/views.py
@@ -1,11 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated
-from .serializers import ServiceSerializer
 
-#@permission_classes([IsAuthenticated])
+from service.models import Service
+from .serializers import ServiceSerializer
+from .scraping import obtener_programacion_eurosport
+
+@permission_classes([IsAuthenticated])
 class ServiceCreateView(APIView):
     def post(self, request):
         serializer = ServiceSerializer(data=request.data)
@@ -13,3 +16,17 @@ class ServiceCreateView(APIView):
             service = serializer.save()
             return Response(serializer.data, status=201)
         return Response(serializer.errors, status=400)
+    
+@permission_classes([IsAuthenticated])
+class ServiceDetailView(APIView):
+    def get(self, request, pk):
+        service = get_object_or_404(Service, pk=pk)
+        serializer = ServiceSerializer(service)
+        return Response(serializer.data)
+    
+@permission_classes([IsAuthenticated])
+class ServiceEventsView(APIView):
+    def get(self, request, pk):
+        service = get_object_or_404(Service, pk=pk)
+        eventos = obtener_programacion_eurosport(service.establishment.platforms)
+        return Response(eventos)
